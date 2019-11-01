bde87001fc25d09982ac19fa46e4fee29c1a6418
Nidhin Harilal <nidhinharilal9@gmail.com>
2019-11-02 02:18:03 +0530

    #6 added

--- check_fs.c
+++ check_fs.c
@@ -16,6 +16,8 @@
 
 int fsfd;
 struct superblock sb;
+
+
 //struct dinode inode;
 /*This checks for corrupted i node */
 int corrupted_inode()        //This code works NO need to mess with it.
@@ -34,6 +36,7 @@ int corrupted_inode()        //This code works NO need to mess with it.
             close(fsfd);
             return 1;
         }
+
         //This checks for error 12
         //No extra links allowed for directories, each directory only appears in one other directory
         if(inode.type == T_DIR && inode.nlink > 1)
@@ -63,6 +66,39 @@ int find_directory_by_name(uint addr, char *name)
         }
     }
     return -1;
+}
+
+/*Error 7 each DAddress must be used only once.*/
+int check_address(uint* address, struct dinode inode)
+{  
+
+    for(int i=0;i<NDIRECT+1;i++){
+
+        if(inode.addrs[i] == 0) {continue;}                              
+        
+        if(address[inode.addrs[i]] == 1) {
+            fprintf(stderr,"ERROR: address used more than once\n");
+            return 1;}     
+        address[inode.addrs[i] ]=1;                                 
+    }
+    
+    int j;
+    uint addr;
+    if(inode.addrs[NDIRECT] != 0){
+        for(j=0; j<NINDIRECT; j++){
+            if (lseek(fsfd, inode.addrs[NDIRECT] * BSIZE + j*sizeof(uint), SEEK_SET) != inode.addrs[NDIRECT] * BSIZE + j*sizeof(uint)){
+                perror("lseek");
+            }
+            if (read(fsfd, &addr, sizeof(uint)) != sizeof(uint)){
+                perror("read");
+            }
+            if(addr==0) {continue;}
+            
+            if(address[addr] == 1) {return 1;}
+            address[addr]=1;
+        }
+    }
+    return 0;
 }
 
 /*error 5*/
@@ -102,16 +138,20 @@ int check_inode_addr(struct dinode inode)
     }
 }
 
+
 /*Check the directories for errors*/
-int check_directory()
+int check_directory(uint *address)
 {
     struct dinode inode;
     int dot_inode=-1;
     int ddot_inode=-1;
     char buf[sizeof(struct dinode)];
-    lseek(fsfd,sb.inodestart*BSIZE,SEEK_SET);
+    lseek(fsfd,sb.inodestart*BSIZE+sizeof(struct dinode),SEEK_SET);
     for (int i=0;i<sb.ninodes;i++)
     {
+        if (check_address(address, inode)){
+            return 1;
+    }
         read(fsfd,buf,sizeof(inode));
         memmove(&inode, buf, sizeof(inode));
         if(inode.type==T_DIR)
@@ -233,40 +273,61 @@ int check_root()
 }
 
 
-/*Error 7 each DAddress must be used only once.*/
-int check_address(uint* addresses)
-{
-    struct dinode inode;
-    for(int i=0;i<NDIRECT+1;i++){                                        
-        if(inode.addrs[i] == 0) {continue;}                              
-        
-        if(addresses[inode.addrs[i]] == 1) {
-            fprintf(stderr,"ERROR: address used more than once\n");
-            return 1;}     
-        addresses[inode.addrs[i] ]=1;                                 
-    }
-    
-    int j;
-    uint address;
-    if(inode.addrs[NDIRECT] != 0){
-        for(j=0; j<NINDIRECT; j++){
-            if (lseek(fsfd, inode.addrs[NDIRECT] * BSIZE + j*sizeof(uint), SEEK_SET) != inode.addrs[NDIRECT] * BSIZE + j*sizeof(uint)){
-                perror("lseek");
-            }
-            if (read(fsfd, &address, sizeof(uint)) != sizeof(uint)){
-                perror("read");
-            }
-            if(address==0) {continue;}
-            
-            if(addresses[address] == 1) {return 1;}
-            addresses[address]=1;
-        }
-    }
-    return 0;
+
+int check_block_inuse(uint* address){
+
+    // Position od datablock in Bitmap
+    // Bitmapstart(in Bytes) + number of bytes in Bitmap(# total blocks%8) - number of bytes of dataBlocks(# dataBlocks%8) 
+	int db_inbmap =sb.bmapstart*BSIZE + sb.size/8 - sb.nblocks/8;
+	
+    // Current address
+	int current_block=(sb.bmapstart + 1);
+
+    // Seeking cursor to the first byte of DataBlock in BitMap
+	lseek(fsfd, db_inbmap, SEEK_SET);
+
+    uint bit_to_check; 
+	int byte_to_check;
+	
+    // taking Bytewise addresses from BitMap
+    for (int i=current_block; i<sb.size; i+=8){
+
+        // reading 1 Byte => it will contain usage info. about 8 DataBlocks
+		read(fsfd, &byte_to_check, 1);
+		for (int x=0; x<8; x++){
+
+            //  Reading last bit step-by-step each time in the corresponding Byte   
+			bit_to_check = (byte_to_check >> x)%2;
+
+            // bit !=0 => when DataBlock marked as in-use in BitMap
+			if (bit_to_check!=0){
+                // address[current_block] is 0 when it is not in use
+				if(address[current_block]==0){
+                    printf("ERROR: bitmap marks block in use but it is not in use.");
+					return 1;
+				}
+			}
+            current_block++;
+		}
+	} 
+	return 0;
 }
 
+
+
+
+
 int main(int argc, char *argv[])
-{
+{   
+
+    
+    uint address[sb.size];
+	// initializing array address with zeores
+    // For error involving blocks-in-use
+    for(int i=0;i<sb.size;i++) {
+		address[i]=0;
+	}
+
     if(argc<2)
     {
         printf("usage $fsck file_system_image.img\n");
@@ -284,6 +345,16 @@ int main(int argc, char *argv[])
     if(check_root()==1)
         return 1;
     //error3 ends
+
+    if(check_directory(address)==1){
+        return 1;
+    }
+
+    // error6 starts
+    if (check_block_inuse(address)){
+        return 1;
+    }
+    // error6 ends
     printf("Your File System is intact\n");
     return 0;
 }
