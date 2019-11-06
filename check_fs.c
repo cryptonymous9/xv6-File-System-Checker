6353478c9c00f07a89cf7438964c9de1d5394b6d
Nidhin Harilal <nidhinharilal9@gmail.com>
2019-11-06 20:52:47 +0530

    error 9

--- check_fs.c
+++ check_fs.c
@@ -32,7 +32,7 @@ int corrupted_inode()        //This code works NO need to mess with it.
         memmove(&inode, buf, sizeof(inode));
         if(inode.type!=0 && inode.type!=T_FILE && inode.type!=T_DIR && inode.type!=T_DEV)
         {
-            printf("ERROR: bad inode.");
+            printf("ERROR: bad inode.\n");
             close(fsfd);
             return 1;
         }
@@ -40,21 +40,21 @@ int corrupted_inode()        //This code works NO need to mess with it.
         //This checks for error 11
         //Refrence counts (number of links) for regular files match the number of times file is referred to in directories (i.e., hard links work correctly)
         //ERROR: bad refrence count for file
-        if(inode.type == T_FILE)
-        {
-            if(inode.nlink != check_links(inode, i))
-            {
-                printf("ERROR: bad reference count for file.");
-                close(fsfd);
-                return 1;
-            }
-        }
+        // if(inode.type == T_FILE)
+        // {
+        //     if(inode.nlink != check_links(inode, i))
+        //     {
+        //         printf("ERROR: bad reference count for file.");
+        //         close(fsfd);
+        //         return 1;
+        //     }
+        // }
 
         //This checks for error 12
         //No extra links allowed for directories, each directory only appears in one other directory
         if(inode.type == T_DIR && inode.nlink > 1)
         {
-            printf("ERROR: directory appears more than once in file system.");
+            printf("ERROR: directory appears more than once in file system.\n");
             close(fsfd);
             return 1;
         }
@@ -92,7 +92,7 @@ int check_address(uint* address, struct dinode inode)
         if(inode.addrs[i] == 0) {continue;}                              
         if(inode.addrs[i]<dstart || inode.addrs[i]>=dstart+sb.nblocks)
         {
-            printf("ERROR: bad direct address in inode.");
+            printf("ERROR: bad direct address in inode.\n");
             return 1;
         }
         if(address[inode.addrs[i]] == 1)
@@ -114,7 +114,7 @@ int check_address(uint* address, struct dinode inode)
             if(addr==0) {continue;}
             if(addr<dstart || addr>=dstart+sb.nblocks)
             {
-                printf("ERROR: bad indirect address in inode.");
+                printf("ERROR: bad indirect address in inode.\n");
                 return 1;
             }
             if(address[addr] == 1)
@@ -142,7 +142,7 @@ int check_inode_addr(struct dinode inode)
         buf=(buf>>offset)%2;
         if(buf==0)
         {
-            printf("ERROR: address used by inode but marked free in bitmap.");
+            printf("ERROR: address used by inode but marked free in bitmap.\n");
             return 1;
         }
     }
@@ -158,7 +158,7 @@ int check_inode_addr(struct dinode inode)
             buf=(buf>>offset)%2;
             if(buf==0)
             {
-                printf("ERROR: address used by inode but marked free in bitmap.");
+                printf("ERROR: address used by inode but marked free in bitmap.\n");
                 return 1;
             }
         }
@@ -224,7 +224,7 @@ int check_directory(uint *address)
                 else
                 {
                     printf("ERROR 3: directory not properly formatted.\n");
-                    printf("%d %d %d\n", dot_inode, ddot_inode, inode.addrs[NDIRECT]);
+                    // printf("%d %d %d\n", dot_inode, ddot_inode, inode.addrs[NDIRECT]);
                     return 1;
                 }
             }
@@ -334,7 +334,7 @@ int check_block_inuse(uint* address){
             if (bit_to_check!=0){
                 // address[current_block] is 0 when it is not in use
                 if(address[current_block]==0){
-                    printf("ERROR: bitmap marks block in use but it is not in use.");
+                    printf("ERROR: bitmap marks block in use but it is not in use.\n");
                     return 1;
                 }
             }
@@ -439,7 +439,62 @@ int check_links(struct dinode current_inode, uint current_inum)
     return count;
 }
 
+// For error 9
+int check_inum_indir(uint addr, ushort inum){   
+    lseek(fsfd, addr*BSIZE, SEEK_SET);
+    struct dirent buf;
+    for(int i=0; i < BSIZE / sizeof(struct dirent); i++){
+        read(fsfd, &buf, sizeof(struct dirent));
+        if(buf.inum==inum){
+            return 0;
+        }
+    }
+    return 1;
+}   
 
+// Error 9:
+int inode_check_directory(uint target_inum){
+
+    // DIR-inode to compare
+    struct dinode compare_inode;
+
+    // loop over all the DIR inodes to find the reference to the in-use target inode
+    for (int compare_inum=0; compare_inum<sb.ninodes; compare_inum++){                                                                          
+
+        // storing the current inode for comparison                                                                       
+        lseek(fsfd, sb.inodestart*BSIZE + compare_inum * sizeof(struct dinode), SEEK_SET);
+        read(fsfd, &compare_inode, sizeof(struct dinode));
+        
+        // 1-> T_DIR
+        // skipping if it is not a Directory
+        if (compare_inode.type!=1) {continue;}                                                                              
+        
+        // looping through all the direct-pointers
+        for(int d_ptr=0; d_ptr<NDIRECT; d_ptr++){
+            // skipping if it is empty
+            if(compare_inode.addrs[d_ptr]==0) {continue;}
+
+            //checking if in-use target inode number present in the compare DIR inode
+            else if(check_inum_indir(compare_inode.addrs[d_ptr], target_inum)==0){return 0;}    
+        }
+        
+        // For all the indirect addresses
+        uint ind_DIR_address;
+        // looinf through all the indirect-pointers
+        for(int ind_ptr=0; ind_ptr<NINDIRECT; ind_ptr++){
+            lseek(fsfd, compare_inode.addrs[NDIRECT] * BSIZE + ind_ptr*sizeof(uint), SEEK_SET);
+            read(fsfd, &ind_DIR_address, sizeof(uint));
+
+            // skipping if empty
+            if(ind_DIR_address==0) {continue;}
+            // checking if in-use target inode number present in the indirect address
+            if(check_inum_indir(ind_DIR_address, target_inum)==0) {return 0;}
+        }
+    }
+    // in-use inode not found in any directory.
+    printf("ERROR: inode marked use but not found in a directory\n");
+    return 1;
+}
 
 int main(int argc, char *argv[])
 {   
@@ -471,15 +526,29 @@ int main(int argc, char *argv[])
         return 1;
     //error3 ends
 
-    if(check_directory(address)==1){
-        return 1;
-    }
+    // if(check_directory(address)==1){
+    //     return 1;
+    // }
 
     // error6 starts
     if (check_block_inuse(address)){
         return 1;
     }
     // error6 ends
+    
+    //error 9
+    // looping through all inodes to check all the in-use inodes
+    for (int current_inum = 0; current_inum < ((int) sb.ninodes); current_inum++){
+        if (inode_check_directory(current_inum)){
+            return 1;
+        }    
+    }
+
+    if(check_directory(address)==1){
+        return 1;
+    }
+
+
     printf("Your File System is intact\n");
     return 0;
 }
